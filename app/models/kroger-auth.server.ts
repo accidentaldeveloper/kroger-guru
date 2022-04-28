e076ac8a1d78bbf6d798e97af20ef8de1efeaa11
Michael Richardson <michael.richardson@kroger.com>
2022-04-28 19:55:11 -0400

    Add access token caching

--- app/models/kroger-auth.server.ts
+++ app/models/kroger-auth.server.ts
@@ -6,7 +6,27 @@ const base64Creds = Buffer.from(
 ).toString("base64");
 const authString = `Basic ${base64Creds}`;
 
-export async function fetchAccessToken(): Promise<string> {
+type ClientCredentialsResponse = {
+  access_token: string;
+  expires_in: number;
+};
+
+type TokenCache = { token: ClientCredentialsToken };
+
+type ClientCredentialsToken = {
+  access_token: string;
+  expiration: number;
+};
+
+const tokenCache = (global.tokenCache ||
+  (global.tokenCache = {
+    token: {
+      access_token: "",
+      expiration: 0,
+    },
+  })) as TokenCache;
+
+async function fetchAccessToken() {
   const startRequest = performance.now();
   const tokenUrl = "https://api.kroger.com/v1/connect/oauth2/token";
   const params = new URLSearchParams({
@@ -30,11 +50,36 @@ export async function fetchAccessToken(): Promise<string> {
     },
     body: params,
   });
-  const data = await res.json();
+  const data = (await res.json()) as ClientCredentialsResponse;
   const requestTime = performance.now() - startRequest;
   console.log(`Token request took ${requestTime}ms`);
   if (!res.ok) {
     throw Error(`Failed to get token: ${JSON.stringify(data)}`);
   }
-  return data.access_token;
+  return data;
 }
+
+const msInOneMinute = 60 * 1000;
+export async function fetchAndSet() {
+  const data = await fetchAccessToken();
+  const { access_token, expires_in } = data;
+  tokenCache.token = {
+    access_token,
+    expiration: Date.now() + expires_in * 1000,
+  };
+  return access_token;
+}
+
+export const getAccessToken = async () => {
+  const { expiration, access_token } = tokenCache.token;
+  const msToExpiration = expiration - Date.now();
+  console.log({ msToExpiration });
+  if (msToExpiration > 0) {
+    if (msToExpiration < msInOneMinute) {
+      fetchAndSet();
+    }
+    return access_token;
+  }
+
+  return await fetchAndSet();
+};
